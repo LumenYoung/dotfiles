19b690ab796229af0a1726f39c8fe94edaab1958
Lumen Yang <lumen.yang@lumeny.io>
2025-10-22 07:36:21 +0200

    nvim: solve deprecation warning

--- nvim/lua/plugins/codecomp.lua
+++ nvim/lua/plugins/codecomp.lua
@@ -46,29 +46,31 @@ return {
       log_level = "DEBUG",
     },
     adapters = {
-      openrouter = function()
-        return require("codecompanion.adapters").extend("openai_compatible", {
-          env = {
-            url = "https://openrouter.ai/api",
-            api_key = "OPENROUTER_API_KEY",
-            chat_url = "/v1/chat/completions",
-          },
-          opts = {
-            stream = true
-          },
-          schema = {
-            model = {
-              default = "google/gemini-2.5-pro",
-              choices = {
-                "google/gemini-2.5-pro",
-                "moonshotai/kimi-k2-0905",
-                "deepseek/deepseek-chat-v3.1",
-                "anthropic/claude-sonnet-4",
+      http = {
+        openrouter = function()
+          return require("codecompanion.adapters").extend("openai_compatible", {
+            env = {
+              url = "https://openrouter.ai/api",
+              api_key = "OPENROUTER_API_KEY",
+              chat_url = "/v1/chat/completions",
+            },
+            opts = {
+              stream = true,
+            },
+            schema = {
+              model = {
+                default = "google/gemini-2.5-pro",
+                choices = {
+                  "google/gemini-2.5-pro",
+                  "moonshotai/kimi-k2-0905",
+                  "deepseek/deepseek-chat-v3.1",
+                  "anthropic/claude-sonnet-4",
+                },
               },
             },
-          },
-        })
-      end,
+          })
+        end,
+      },
     },
   },
 }
