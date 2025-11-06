215f90161f02c53dcf256847a2c6ab785aee7d04
Lumen Yang <lumen.yang@lumeny.io>
2025-11-06 10:28:08 +0100

    nvim: mason ensure installed

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -12,6 +12,11 @@ return {
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
         "lua_ls",
+        "tinymist",
+        "basedpyright",
+        "stylua",
+        "ruff",
+        "clangd"
         -- add more arguments for adding more language servers
       })
     end,
