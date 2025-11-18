3b3db9fb229e2fbd41a42cd1a2e5be243a853249
Lumen Yang <lumen.yang@lumeny.io>
2025-11-18 20:55:45 +0100

    Revert "nvim: aider integration"
    This reverts commit 76142ae7cda61e6f64de758b4ebe6162ee58d7f3.
    
    The aider plugin can't make basic works

--- nvim/lua/plugins/aider.lua
+++ nvim/lua/plugins/aider.lua
@@ -1,114 +1,51 @@
 return {
   {
-    "GeorgesAlkhouri/nvim-aider",
-    cmd = "Aider",
-    dependencies = {
-      { "folke/snacks.nvim", version = ">=2.24.0" },
-      -- Optional themes/integrations
-      "catppuccin/nvim",
-      {
-        "nvim-neo-tree/neo-tree.nvim",
-        opts = function(_, opts)
-          -- Let nvim-aider extend Neo-tree mappings (+, -, =) as documented
-          require("nvim_aider.neo_tree").setup(opts)
-        end,
-      },
-    },
-    -- Use the recommended default configuration from the README
-    opts = {
-      -- Command that executes Aider
-      aider_cmd = "aider",
-      -- Command line arguments passed to aider
-      args = {
-        "--no-auto-commits",
-        "--pretty",
-        "--stream",
-      },
-      -- Automatically reload buffers changed by Aider (requires vim.o.autoread = true)
-      auto_reload = false,
-      -- Idle timeout in ms for Aider's output.
-      idle_timeout = 5000,
-      -- Response timeout in ms for Aider's first output chunk.
-      response_timeout = 30000,
-      -- Timeout in ms for quick commands.
-      quick_idle_timeout = 500,
-      -- A list of slash-commands that should have a shorter idle timeout.
-      quick_commands = {
-        "/add",
-        "/drop",
-        "/read-only",
-        "/ls",
-        "/clear",
-        "/reset",
-        "/undo",
-      },
-      -- Show 'Processing...' and 'Done' notifications.
-      notifications = true,
-      -- Theme colors (automatically uses Catppuccin flavor if available)
-      theme = {
-        user_input_color = "#a6da95",
-        tool_output_color = "#8aadf4",
-        tool_error_color = "#ed8796",
-        tool_warning_color = "#eed49f",
-        assistant_output_color = "#c6a0f6",
-        completion_menu_color = "#cad3f5",
-        completion_menu_bg_color = "#24273a",
-        completion_menu_current_color = "#181926",
-        completion_menu_current_bg_color = "#f4dbd6",
-      },
-      -- snacks.picker.layout.Config configuration
-      picker_cfg = {
-        preset = "vscode",
-      },
-      -- Other snacks.terminal.Opts options
-      config = {
-        os = { editPreset = "nvim-remote" },
-        gui = { nerdFontsVersion = "3" },
-      },
-      win = {
-        wo = { winbar = "Aider" },
-        style = "nvim_aider",
-        position = "float",
-      },
-    },
+    "akinsho/toggleterm.nvim",
+    lazy = true,
   },
   {
     "AstroNvim/astrocore",
+    dependencies = { "akinsho/toggleterm.nvim" },
     opts = function(_, opts)
-      opts.mappings = opts.mappings or {}
-
-      local keymaps = {
-        n = {
-          ["<leader>a/"] = { "<cmd>Aider toggle<cr>", desc = "Toggle Aider" },
-          ["<leader>as"] = { "<cmd>Aider send<cr>", desc = "Send to Aider" },
-          ["<leader>ac"] = { "<cmd>Aider command<cr>", desc = "Aider Commands" },
-          ["<leader>ab"] = { "<cmd>Aider buffer<cr>", desc = "Send Buffer" },
-          ["<leader>a+"] = { "<cmd>Aider add<cr>", desc = "Add File" },
-          ["<leader>a-"] = { "<cmd>Aider drop<cr>", desc = "Drop File" },
-          ["<leader>ar"] = { "<cmd>Aider add readonly<cr>", desc = "Add Read-Only" },
-          ["<leader>aR"] = { "<cmd>Aider reset<cr>", desc = "Reset Session" },
-          ["<C-a>"] = {
-            function() require("nvim_aider").api.toggle_terminal() end,
-            desc = "Toggle Aider",
-          },
+      local Terminal = require("toggleterm.terminal").Terminal
+      local aider = Terminal:new {
+        cmd = "aider",
+        hidden = true,
+        direction = "float",
+        count = 99,
+        float_opts = {
+          border = "double",
         },
-        v = {
-          ["<leader>as"] = { "<cmd>Aider send<cr>", desc = "Send to Aider" },
-        },
-        t = {
-          ["<C-a>"] = {
-            function() require("nvim_aider").api.toggle_terminal() end,
-            desc = "Toggle Aider",
-          },
-        },
+        on_open = function(term)
+          vim.cmd "startinsert!"
+          vim.api.nvim_buf_set_keymap(term.bufnr, "t", "<C-v>", "<C-\\><C-n>", { noremap = true, silent = true })
+          vim.api.nvim_buf_set_keymap(
+            term.bufnr,
+            "t",
+            "<C-\\>",
+            "<cmd>lua _G.toggle_aider()<CR>",
+            { noremap = true, silent = true }
+          )
+          vim.api.nvim_buf_set_keymap(
+            term.bufnr,
+            "t",
+            "<C-a>",
+            "<cmd>lua _G.toggle_aider()<CR>",
+            { noremap = true, silent = true }
+          )
+        end,
       }
 
-      for mode, mappings in pairs(keymaps) do
-        opts.mappings[mode] = opts.mappings[mode] or {}
-        for key, value in pairs(mappings) do
-          opts.mappings[mode][key] = value
-        end
-      end
+      _G.toggle_aider = function() aider:toggle() end
+
+      opts.mappings = opts.mappings or {}
+      opts.mappings.n = opts.mappings.n or {}
+      opts.mappings.n["<leader>ta"] = { _G.toggle_aider, desc = "Toggle Aider" }
+      opts.mappings.n["<C-a>"] = { _G.toggle_aider, desc = "Toggle Aider" }
+
+      -- Add toggleterm normal mode mapping for Aider
+      opts.mappings.t = opts.mappings.t or {}
+      opts.mappings.t["<C-a>"] = { "<cmd>lua _G.toggle_aider()<CR>", desc = "Toggle Aider" }
 
       return opts
     end,
